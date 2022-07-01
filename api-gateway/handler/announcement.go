bf1692c12ffc4a08406de3b6723624d9bd797a84
miello <partearth1212@gmail.com>
2022-07-01 17:58:55 +0700

    Implement get announcement handler

--- api-gateway/handler/announcement.go
+++ api-gateway/handler/announcement.go
@@ -1,8 +1,11 @@
 package handler
 
 import (
+	"api-gateway/models"
 	"api-gateway/services/scraper"
+	"api-gateway/utils"
 
+	"github.com/gofiber/fiber/v2"
 	"gorm.io/gorm"
 )
 
@@ -14,4 +17,27 @@ func NewAnnouncementHandler(db *gorm.DB) *AnnouncementHandler {
 	return &AnnouncementHandler{
 		service: scraper.NewAnnouncementService(db),
 	}
+}
+
+func (h *AnnouncementHandler) GetAnnouncement(c *fiber.Ctx) error {
+	id := c.Params("id")
+
+	if id == "" {
+		c.JSON(&models.ResponseError{
+			Msg: "Required course id",
+		})
+		return c.SendStatus(400)
+	}
+
+	res, err := h.service.GetAnnouncements(id)
+	if err != nil {
+		status_code, body := utils.ExtractError(err)
+
+		c.JSON(body)
+		return c.SendStatus(status_code)
+	}
+
+	c.JSON(res)
+
+	return nil
 }
--- api-gateway/services/scraper/announcement.go
+++ api-gateway/services/scraper/announcement.go
@@ -1,15 +1,61 @@
 package scraper
 
 import (
+	"api-gateway/models"
+	"api-gateway/utils"
+	"time"
+
 	"gorm.io/gorm"
 )
 
 type AnnouncementService struct {
-	db *gorm.DB
+	db            *gorm.DB
+	courseService *CourseService
 }
 
 func NewAnnouncementService(db *gorm.DB) *AnnouncementService {
 	return &AnnouncementService{
-		db,
+		db:            db,
+		courseService: NewCourseService(db),
 	}
+}
+
+func convertToAnnouncementView(announcement *models.Announcement) models.AnnouncementView {
+	return models.AnnouncementView{
+		Title: announcement.Title,
+		Href:  announcement.Href,
+		Date:  announcement.PublishDate.Format(time.RFC3339),
+	}
+}
+
+func (c *AnnouncementService) GetAnnouncements(id string) ([]models.AnnouncementView, error) {
+	found, err := c.courseService.IsCourseIdExists(id)
+
+	if err != nil {
+		return nil, utils.CreateError(500, err.Error())
+	}
+
+	if !found {
+		return nil, utils.CreateError(404, "Not found, maybe api owner does not attend this course")
+	}
+
+	query := models.Announcement{
+		ID: id,
+	}
+
+	var raw_announcement []models.Announcement
+
+	tx := c.db.Where(&query).Find(&raw_announcement)
+
+	if tx.Error != nil {
+		return nil, utils.CreateError(500, tx.Error.Error())
+	}
+
+	var announcement_view []models.AnnouncementView
+
+	for _, announcement := range raw_announcement {
+		announcement_view = append(announcement_view, convertToAnnouncementView(&announcement))
+	}
+
+	return announcement_view, nil
 }
