d9f380904bb69175e38066a1e8af8e1eeec104af
miello <partearth1212@gmail.com>
2022-07-03 16:29:54 +0700

    Implement materials handler

--- discord-bot/src/commands/material.ts
+++ discord-bot/src/commands/material.ts
@@ -1,11 +1,50 @@
-import { SlashCommandBuilder } from '@discordjs/builders'
-import { CacheType, CommandInteraction } from 'discord.js'
+import { hyperlink, SlashCommandBuilder } from '@discordjs/builders'
+import {
+  CacheType,
+  CommandInteraction,
+  EmbedFieldData,
+  MessageEmbed,
+} from 'discord.js'
 import { apiClient } from '../config/axios'
 import { ICommand } from '../types/command'
+import { IMaterial } from '../types/material'
+import { extractInteractiveInfo } from '../utils/misc'
 
 const execute = async (interaction: CommandInteraction<CacheType>) => {
-  const resp = await apiClient.get('/materials')
-  await interaction.reply({ content: 'Material' })
+  const [courseId, title] = extractInteractiveInfo(interaction)
+  const resp = await apiClient.get<Array<IMaterial>>(`/${courseId}/materials`)
+
+  const materials = resp.data
+
+  const embed = new MessageEmbed()
+  embed.setTitle(`${title} Materials`)
+  embed.setThumbnail(
+    'https://images-ext-2.discordapp.net/external/4Q85mjDG7508BRnWbBibIMLsL1QYffvT7aq5b4HDaxM/https/www.mycourseville.com/sites/all/modules/courseville/files/logo/cv-logo.png'
+  )
+  embed.setColor('AQUA')
+
+  const embedList: EmbedFieldData[] = []
+
+  if (materials) {
+    materials.forEach((val) => {
+      const { folderName, file } = val
+
+      embedList.push({
+        name: folderName,
+        value: file
+          .splice(0, 4)
+          .map((val) => {
+            const { title, href } = val
+            return hyperlink(title, href)
+          })
+          .join('\n'),
+      })
+    })
+  }
+
+  embed.addFields(...embedList)
+
+  await interaction.reply({ embeds: [embed] })
 }
 
 export default {
