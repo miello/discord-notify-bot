15e904f089b9d658f82473cbfff7ca14b4995d3e
miello <partearth1212@gmail.com>
2022-07-03 11:59:06 +0700

    Implement slash command

--- discord-bot/src/env.ts
+++ discord-bot/src/env.ts
@@ -4,5 +4,6 @@ config()
 
 const DISCORD_TOKEN = process.env.DISCORD_TOKEN || ''
 const APPLICATION_ID = process.env.APPLICATION_ID || ''
+const BASE_API_GATEWAY = process.env.BASE_API_GATEWAY || ''
 
-export { DISCORD_TOKEN, APPLICATION_ID }
+export { DISCORD_TOKEN, APPLICATION_ID, BASE_API_GATEWAY }
--- discord-bot/src/index.ts
+++ discord-bot/src/index.ts
@@ -1,27 +1,6 @@
 import { Client, Intents } from 'discord.js'
-import { Routes } from 'discord-api-types/v9'
-import { SlashCommandBuilder } from '@discordjs/builders'
-import { APPLICATION_ID, DISCORD_TOKEN } from './env'
-import { REST } from '@discordjs/rest'
-
-const commands = [
-  new SlashCommandBuilder()
-    .setName('ping')
-    .setDescription('Replies with pong!'),
-  new SlashCommandBuilder()
-    .setName('server')
-    .setDescription('Replies with server info!'),
-  new SlashCommandBuilder()
-    .setName('user')
-    .setDescription('Replies with user info!'),
-].map((command) => command.toJSON())
-
-const rest = new REST({ version: '9' }).setToken(DISCORD_TOKEN)
-
-rest
-  .put(Routes.applicationCommands(APPLICATION_ID), { body: commands })
-  .then(() => console.log('Successfully registered application commands.'))
-  .catch(console.error)
+import { DISCORD_TOKEN } from './env'
+import { updateSlashCommand } from './slash_command'
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] })
 
@@ -46,5 +25,7 @@ client.on('interactionCreate', async (interaction) => {
     )
   }
 })
+
+updateSlashCommand()
 
 client.login(DISCORD_TOKEN)
